5bfaa2cd6c2d2e6a8f68ea118fd425253440dedc
Dzmitry Kashlach <2155941+dzmitrykashlach@users.noreply.github.com>
2022-10-25 11:44:23 +0300

    BodyInserters.fromProducer

--- src/main/java/com/dzmitrykashlach/sakila/SakilaConfig.java
+++ src/main/java/com/dzmitrykashlach/sakila/SakilaConfig.java
@@ -5,14 +5,9 @@ import com.dzmitrykashlach.sakila.components.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.Objects;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -24,17 +19,10 @@ public class SakilaConfig {
 
     @Bean
     RouterFunction<ServerResponse> getPaymentRoute() {
-        return route(GET("/payments"), paymentFunction());
+        return route(GET("/payments"),
+                req -> ServerResponse.ok().body(BodyInserters.fromProducer(
+                        paymentRepository.findAll(), Payment.class))
+        );
     }
 
-
-    private HandlerFunction paymentFunction() {
-        ResponseEntity<Mono<List<Payment>>> listServerResponse =
-                ResponseEntity.ok().body(
-                        paymentRepository.findAll().collectList()
-                );
-
-        return req -> Objects.requireNonNull(
-                listServerResponse.getBody());
-    }
 }
