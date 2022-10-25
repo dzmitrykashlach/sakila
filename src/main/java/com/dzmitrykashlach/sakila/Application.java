d29479b0b8e309afd72fbdde93f612bd27fcb560
Dzmitry Kashlach <2155941+dzmitrykashlach@users.noreply.github.com>
2022-10-25 17:01:30 +0300

    Payment record

--- src/main/java/com/dzmitrykashlach/sakila/Application.java
+++ src/main/java/com/dzmitrykashlach/sakila/Application.java
@@ -1,15 +1,29 @@
 package com.dzmitrykashlach.sakila;
 
+import com.dzmitrykashlach.sakila.components.Payment;
 import com.dzmitrykashlach.sakila.components.PaymentRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import java.util.List;
 
 @SpringBootApplication
-@Slf4j
-public class Application {
+public class Application implements CommandLineRunner {
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private RouterFunction<ServerResponse> routerFunction;
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Payment> paymentList = paymentRepository.findAll().collectList().block();
+        paymentList.get(0);
     }
 }
--- src/main/java/com/dzmitrykashlach/sakila/components/Payment.java
+++ src/main/java/com/dzmitrykashlach/sakila/components/Payment.java
@@ -1,38 +1,17 @@
 package com.dzmitrykashlach.sakila.components;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-public class Payment {
-    @Id
-    @Column(value = "payment_id")
-    private int id;
-
-    @Column(value = "customer_id")
-    private int customerId;
-
-    @Column(value = "staff_id")
-    private int staffId;
-
-    @Column(value = "rental_id")
-    private int rentalId;
-
-    @Column
-    private int amount;
-
-    @Column(value = "payment_date")
-    private LocalDateTime paymentDate;
-
-    @Column(value = "last_update")
-    private LocalDateTime lastUpdate;
-}
+public record Payment(
+        @Id
+        @Column(value = "payment_id") int id,
+        @Column(value = "customer_id") int customerId,
+        @Column(value = "staff_id") int staffId,
+        @Column(value = "rental_id") int rentalId,
+        @Column int amount,
+        @Column(value = "payment_date") LocalDateTime paymentDate,
+        @Column(value = "last_update") LocalDateTime lastUpdate
+) {}
