6d63a59ae21cf5967091ee80f370a9430b21212a
Dzmitry Kashlach <2155941+dzmitrykashlach@users.noreply.github.com>
2022-11-09 13:30:39 +0300

    V2__addPayments.sql

--- mvc/src/main/java/com/mvc/Application.java
+++ mvc/src/main/java/com/mvc/Application.java
@@ -1,22 +1,41 @@
 package com.mvc;
 
+import com.mvc.components.Payment;
+import com.mvc.components.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Random;
 
-@SpringBootApplication(scanBasePackages={"com.mvc.components"})
+
+@SpringBootApplication(scanBasePackages = {"com.mvc.components"})
 @ComponentScan({"com.mvc.components"})
 public class Application implements CommandLineRunner {
 
+
     @Autowired
+    PaymentRepository paymentRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Random random = new Random();
+        // SELECT * from payment p  order by payment_id  DESC ;
+        long paymentSize = paymentRepository.findAll().spliterator().getExactSizeIfKnown();
+        while (paymentSize < 65528) {
+            Payment payment = new Payment(599, 1, 514, random.nextFloat(),
+                    LocalDateTime.of(2022, Month.APRIL, 16, 12, 34)
+            );
+            paymentRepository.save(payment);
+            paymentSize++;
+        }
     }
 }
--- mvc/src/main/java/com/mvc/components/Payment.java
+++ mvc/src/main/java/com/mvc/components/Payment.java
@@ -1,17 +1,43 @@
 package com.mvc.components;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public record Payment(
-        @Id
-        @Column(value = "payment_id") int id,
-        @Column(value = "customer_id") int customerId,
-        @Column(value = "staff_id") int staffId,
-        @Column(value = "rental_id") int rentalId,
-        @Column int amount,
-        @Column(value = "payment_date") LocalDateTime paymentDate,
-        @Column(value = "last_update") LocalDateTime lastUpdate
-) {}
+@Entity
+@Data
+public class Payment
+ {
+         @Id
+         @GeneratedValue
+         @Column(value = "payment_id")
+         public int id;
+         @Column(value = "customer_id")
+         public int customerId;
+         @Column(value = "staff_id")
+         public int staffId;
+         @Column(value = "rental_id")
+         public int rentalId;
+         @Column
+         public float amount;
+         @Column(value = "payment_date")
+         public LocalDateTime paymentDate;
+         @Column(value = "last_update")
+         public LocalDateTime lastUpdate;
+
+         public Payment(int customerId,int staffId,int rentalId,float amount,
+                       LocalDateTime paymentDate){
+                this.customerId=customerId;
+                this.staffId=staffId;
+                this.rentalId=rentalId;
+                this.amount=amount;
+                Objects.requireNonNull(paymentDate);
+                this.lastUpdate = LocalDateTime.now();
+        }
+
+}
